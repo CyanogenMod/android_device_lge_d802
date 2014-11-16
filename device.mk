c2e61e918a1122f8264145242b570229a47de7ea
Rashed Abdel-Tawab <rashed@linux.com>
2014-11-15 21:19:51 -0500

    d802: Move libnfc-brcm.conf to device and update for 5.0
    Change-Id: I9e50c58a52b1ba45c4f52540d5ea04cfad01f1ed

--- device.mk
+++ device.mk
@@ -32,3 +32,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     nfc_nci.bcm2079x.default \
     NfcNci
+
+# NFC config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
+

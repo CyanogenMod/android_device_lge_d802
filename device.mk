48f24c5a38cfe9e61ede5c86f98926017929e9b4
Rashed Abdel-Tawab <rashed@linux.com>
2015-03-10 13:09:12 -0400

    d802: Add variant specific sensor configuration
    Change-Id: I1f1e5b79f88e247aa506e70f292b05b64b401eb7

--- device.mk
+++ device.mk
@@ -22,3 +22,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # NFC
 $(call inherit-product, device/lge/g2-common/nfc.mk)
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensor_def_d802.conf:system/etc/sensor_def_variable.conf

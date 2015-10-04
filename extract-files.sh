5b6cf78b9a8cc9f0b4627fc1ca2a99bf99167b38
Ethan Chen <intervigil@gmail.com>
2015-10-03 17:12:17 -0700

    d802: Fix proprietary file extraction
    Change-Id: Ia708b4989865bd3c443718375d0438b13ead300c

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-VENDOR=lge
-DEVICE=d802
+set -e
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ `; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
-done
-
-./setup-makefiles.sh
+export VENDOR=lge
+export DEVICE=d802
+./../../$VENDOR/g2-common/extract-files.sh $@

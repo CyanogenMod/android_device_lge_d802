c3b2574207d20f9afd00b8682d80d6fe3806e145
Rashed Abdel-Tawab <rashed@linux.com>
2015-11-19 16:36:13 -0500

    d802: Add assertions against trustzone and bootloader
    Change-Id: Ibce77474ee323248cd72eca1b4c71a90efb081ed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,9 @@ include device/lge/g2-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_CONFIG := cyanogenmod_d802_defconfig
 
-TARGET_OTA_ASSERT_DEVICE := d802,g2,galbi,d800
+TARGET_OTA_ASSERT_DEVICE := d802,g2,galbi
 
 G2_DTS_TARGET := msm8974-g2-open_com
 
+# Assertions
+TARGET_BOARD_INFO_FILE ?= device/lge/d802/board-info.txt
